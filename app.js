0b3342d9655bcb4339c2e94d357522ecb6f80b52
Max <maxime.leb@gmail.com>
2022-12-16 11:39:44 +0100

    header

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ var touitsRouter = require('./routes/touits');
 var app = express();
 
 const cors = require('cors');
-app.use(cors({ "Access-Control-Allow-Origin": "*" }));
+app.use(cors({ 
+    origin:'*'
+}))
+app.options('*', cors())
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Content-Type', 'Accept')
+    next()
+})
 
 app.use(logger('dev'));
 app.use(express.json());
